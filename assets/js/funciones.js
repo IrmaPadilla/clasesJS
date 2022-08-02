1d8fe1eb1c7a9832a77db9ef6dad7e7ebe1a8df6
IrmaPadilla <irmapaulinapadilla@gmail.com>
2022-08-02 16:22:09 -0500

    Update notes

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -6,20 +6,26 @@
 
 
 //Sintaxis básica para declarar una función
-function pauFunction(){
+function pauFunction() {
     //Dentro de las llaves va lo que va a hacer la función
     console.log("Hola mundo");
-
-
 }
 //Sintaxis básica para usar esa función declarada previamente
 pauFunction();
 
-
-
-function myName(){
-    let name = PauMeza;
+function myName() {
+    let name = "PauMeza";
     console.log(name);
 }
 
-function myName();
+myName();
+
+
+var nombre = "paulina";
+
+function globalScoope(){
+    console.log(nombre);
+
+}
+
+globalScoope();
