50621406e0feb16ec2dcb10fdaae5002fc8900aa
IrmaPadilla <irmapaulinapadilla@gmail.com>
2022-08-01 20:27:56 -0500

    Primeras notas en JS

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//alert ("Hola Pau, excelente día");
+//Este es un comentario de una sola línea
+/*Comentario multilínea*/
+
+var nombre = prompt("Hola, ¿Cómo te llamas?");
+
+console.log(nombre);
+
+
+
+var base = prompt("Ingrese la base del triangulo");
+var altura = prompt("Ingrese la base de la altura");
+
+var areaTriangulo = ((base*altura)/2);
+
+document.write("Hola, " + nombre + ". El área del triángulo es: " + areaTriangulo);
---
+++ assets/js/variables.js
@@ -0,0 +1,40 @@
+//Mejores prácticas
+/*Camel case */
+//Palabra reservada
+
+/*var while;
+var null ;
+var if;
+var true;
+var false;
+var var;
+var const;
+var let;
+*/
+//Declaración de variables, type number
+var edad = 23;
+var altura = 166;
+
+//Type boolean
+
+var hambre = true;
+var bateria = true;
+
+//Type syting
+var nombre = "Pau";
+var nacionalidad = "Mexicana";
+var ciudad = "Sinaloa";
+
+//Type null
+var ganasDeBibir = null;
+var briAmonestaciones = null;
+
+//Type undefined
+var xx, yy, zz;
+
+//Tarea 01/agosto/2022
+//Escribir en una sola línea variables undefined de nuestro carrito de compras
+
+
+
+
